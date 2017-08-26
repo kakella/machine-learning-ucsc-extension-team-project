5f9270d8a7242e80b1cd617f8f595e589642be66
Ksheerasagar Akella <Ksheerasagar.Akella@amdocs.com>
2017-08-26 11:30:28 -0700

    Minor refactorings: - made some methods private - better names

--- algorithms/k_means_clustering.py
+++ algorithms/k_means_clustering.py
@@ -6,29 +6,29 @@ class KMeansClustering:
     def __init__(self, stop_threshold=None, n_iterations=None):
         self.stop_threshold = stop_threshold or 0.1
         self.n_iterations = n_iterations or 10000
-        self.classifier_means = None
+        self.classifier = None
 
     @staticmethod
-    def init_means(nd_data, k):
+    def __init_means(nd_data, k):
         t_shape = nd_data.shape
         v_min = np.array([min(nd_data[:, i]) for i in range(t_shape[1])])
         v_max = np.array([max(nd_data[:, i]) for i in range(t_shape[1])])
         return np.random.uniform(v_min, v_max, (k, t_shape[1]))
 
     @staticmethod
-    def per_mean_iterate(nd_data, v_mean):
+    def __per_mean_iterate(nd_data, v_mean):
         return [sum((feature - v_mean) ** 2) for feature in nd_data]
 
     @staticmethod
-    def calculate_errors(nd_data, nd_means):
-        return [KMeansClustering.per_mean_iterate(nd_data, v_mean) for v_mean in nd_means]
+    def __calculate_errors(nd_data, nd_means):
+        return [KMeansClustering.__per_mean_iterate(nd_data, v_mean) for v_mean in nd_means]
 
     @staticmethod
-    def find_closest_mean(nd_errors):
+    def __find_closest_mean(nd_errors):
         return np.argmin(nd_errors, axis=0)
 
     @staticmethod
-    def safe_divide(arr1, arr2):
+    def __safe_divide(arr1, arr2):
         out = []
         a1 = arr1.flatten()
         l1 = len(a1)
@@ -45,7 +45,7 @@ class KMeansClustering:
         return np.array(out).reshape(arr1.shape)
 
     @staticmethod
-    def recalculate_means(nd_data, nd_closest_means, k):
+    def __recalculate_means(nd_data, nd_closest_means, k):
         t_shape = nd_data.shape
         nd_means = np.zeros((k, t_shape[1]))
 
@@ -54,19 +54,19 @@ class KMeansClustering:
             nd_means[index] += nd_data[i]
             v_mean_feature_counts[index] += 1
 
-        nd_means = KMeansClustering.safe_divide(nd_means, v_mean_feature_counts)
+        nd_means = KMeansClustering.__safe_divide(nd_means, v_mean_feature_counts)
         return nd_means
 
     @staticmethod
-    def calculate_means_delta(old_means, new_means):
+    def __calculate_means_delta(old_means, new_means):
         delta = 0
-        diff = np.square(KMeansClustering.safe_divide((new_means - old_means), old_means))
+        diff = np.square(KMeansClustering.__safe_divide((new_means - old_means), old_means))
         for mean in diff:
             delta += sum(mean)
         return delta
 
     def train(self, nd_data, k):
-        nd_means = self.init_means(nd_data, k)
+        nd_means = KMeansClustering.__init_means(nd_data, k)
         recalculated_means = None
 
         means_tracker = nd_means
@@ -74,10 +74,10 @@ class KMeansClustering:
 
         for i in range(1, self.n_iterations):
             iteration_tracker += 1
-            nd_errors = KMeansClustering.calculate_errors(nd_data, nd_means)
-            nd_closest_means = KMeansClustering.find_closest_mean(nd_errors)
-            recalculated_means = KMeansClustering.recalculate_means(nd_data, nd_closest_means, k)
-            means_delta = float(KMeansClustering.calculate_means_delta(nd_means, recalculated_means))
+            nd_errors = KMeansClustering.__calculate_errors(nd_data, nd_means)
+            nd_closest_means = KMeansClustering.__find_closest_mean(nd_errors)
+            recalculated_means = KMeansClustering.__recalculate_means(nd_data, nd_closest_means, k)
+            means_delta = float(KMeansClustering.__calculate_means_delta(nd_means, recalculated_means))
 
             if means_delta <= self.stop_threshold:
                 break
@@ -85,12 +85,12 @@ class KMeansClustering:
                 nd_means = recalculated_means
                 means_tracker = np.concatenate((means_tracker, nd_means))
 
-        self.classifier_means = recalculated_means
+        self.classifier = recalculated_means
         return means_tracker, iteration_tracker
 
     def classify(self, v_query):
-        nd_errors = KMeansClustering.calculate_errors(np.array([v_query]), self.classifier_means)
-        return KMeansClustering.find_closest_mean(nd_errors)[0]
+        nd_errors = KMeansClustering.__calculate_errors(np.array([v_query]), self.classifier)
+        return KMeansClustering.__find_closest_mean(nd_errors)[0]
 
 
 def main():
@@ -102,7 +102,7 @@ def main():
         classifier = KMeansClustering()
         successive_means, n_iterations = classifier.train(nd_data, k)
         print('classifier created in [%s] iterations' % n_iterations)
-        print('classifier:\n', classifier.classifier_means)
+        print('classifier:\n', classifier.classifier)
         # print('successive means:\n', successive_means)
 
         v_query = np.random.random_sample(2)
